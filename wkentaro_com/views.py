3c9eb7b22e55912ec580afd49a69172f0656b2b9
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-06-22 04:21:19 +0900

    Simplify github software stats

--- wkentaro_com/views.py
+++ wkentaro_com/views.py
@@ -26,16 +26,28 @@ def research(request):
 def software(request):
     template_name = 'software.html'
 
-    GITHUB_USERNAME = 'wkentaro'
-    GITHUB_PASSWORD = os.environ.get('GITHUB_PASSWORD')
-    repo_stats_cache = cache.get('repo_stats') or (datetime(1970, 1, 1, 0, 0, 0), None)
-    if repo_stats_cache[0] < datetime.now() - timedelta(days=1):
-        repo_stats = get_contributed_repos(GITHUB_USERNAME,
-                                           GITHUB_PASSWORD,
-                                           skip_owners=['utmi-2014'])
-        cache.set('repo_stats', (datetime.now(), repo_stats))
-    else:
-        repo_stats = repo_stats_cache[1]
+    repos = [
+        'pfnet/chainer',
+        'pfnet/cupy',
+        'ros/ros_comm',
+        'ros/ros',
+        'start-jsk/jsk_apc',
+        'jsk-ros-pkg/jsk_common',
+        'jsk-ros-pkg/jsk_recognition',
+        'jsk-ros-pkg/jsk_visualization',
+    ]
+    repos = [repo.split('/') for repo in repos]
 
-    return render(request, template_name, {'page_name': 'software',
-                                           'repo_stats': repo_stats[:20]})
+    # GITHUB_USERNAME = 'wkentaro'
+    # GITHUB_PASSWORD = os.environ.get('GITHUB_PASSWORD')
+    # repo_stats_cache = cache.get('repo_stats') or (datetime(1970, 1, 1, 0, 0, 0), None)
+    # if repo_stats_cache[0] < datetime.now() - timedelta(days=1):
+    #     repo_stats = get_contributed_repos(GITHUB_USERNAME,
+    #                                        GITHUB_PASSWORD,
+    #                                        skip_owners=['utmi-2014'])
+    #     cache.set('repo_stats', (datetime.now(), repo_stats))
+    # else:
+    #     repo_stats = repo_stats_cache[1]
+
+    return render(request, template_name,
+                  {'page_name': 'software', 'repos': repos})
