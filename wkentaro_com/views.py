1f696eee1b280e5a945ca01df8730c25a59e85dc
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-11-04 02:21:31 +0000

    Use Javascript to get Github Stats

--- wkentaro_com/views.py
+++ wkentaro_com/views.py
@@ -5,8 +5,6 @@ import os
 import os.path as osp
 
 import flask
-import github3
-import gotenshita
 import jinja2
 import pytz
 from werkzeug.contrib.cache import SimpleCache
@@ -34,10 +32,6 @@ def about():
 
 @app.route('/software')
 def software():
-    token = os.environ.get('GITHUB_TOKEN')
-    if token is None:
-        return flask.redirect('https://github.com/wkentaro')
-
     # get repos from github api
     repos = cache.get('repos')
     if repos is None:
@@ -86,17 +80,6 @@ def software():
                 'wkentaro/gotenshita',
             ]),
         ])
-        gh = github3.login(token=token)
-        for desc, slugs in repos.items():
-            for i, slug in enumerate(slugs):
-                owner, name = slug.split('/')
-                repo = gh.repository(owner, name)
-                repo._my_contributions = None
-                for c in repo.iter_contributor_statistics():
-                    if c.author.login == 'wkentaro':
-                        repo._my_contributions = c.total
-                slugs[i] = repo
-        cache.set('repos', repos, timeout=60 * 60)  # 1h
 
     colors_json = osp.join(
         app.static_folder, 'resource/github-colors/colors.json')
@@ -118,21 +101,6 @@ def projects(project_name):
         )
     except jinja2.exceptions.TemplateNotFound:
         return flask.redirect('/')
-
-
-@app.route('/projects/gotenshita')
-def projects_gotenshita():
-    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
-    if gotenshita.is_gotenshita_open(now, verbose=False):
-        info = gotenshita.get_open_info_monthly(now)
-        daily_info = []
-        for period, open_status in sorted(info[now.day].items()):
-            if datetime.datetime.strptime(period[0], '%H:%M').hour >= now.hour:
-                daily_info.append((period, open_status))
-    else:
-        daily_info = []
-    return flask.render_template('projects/gotenshita.html',
-                                 now=now, info=daily_info)
 
 
 # -----------------------------------------------------------------------------
