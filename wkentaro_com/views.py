053e2ad5befbd1d0b048abf3584cdb805da6199f
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-02-13 16:28:59 +1000

    Add research.yaml

--- wkentaro_com/views.py
+++ wkentaro_com/views.py
@@ -16,6 +16,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
+    filename = osp.join(here, 'data/research.yaml')
+    with open(filename) as f:
+        papers = yaml.load(f)['papers']
+        papers = [paper for paper in papers if paper['selected']]
+
     try:
         timestamp = subprocess.check_output(
             'git log -1 --format="%at" templates/include/updates.html',
@@ -28,13 +33,19 @@ def index():
         updated_at = None
 
     return flask.render_template(
-        'index.html', name='index', updated_at=updated_at
+        'index.html', name='index', updated_at=updated_at, papers=papers
     )
 
 
 @app.route('/research')
 def research():
-    return flask.render_template('research.html', name='research')
+    filename = osp.join(here, 'data/research.yaml')
+    with open(filename) as f:
+        papers = yaml.load(f)['papers']
+
+    return flask.render_template(
+        'research.html', name='research', papers=papers
+    )
 
 
 @app.route('/software')
